934163077d0f3d9ba8843a7e607442584e57a690
Fernando <naando.araujo@gmail.com>
2022-07-02 10:56:26 +1200

    Added dependency injection classes

---
+++ AmethystWindows/DependencyInjection/IocProvider.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace AmethystWindows.DependencyInjection
+{
+    public class IocProvider
+    {
+        private static IServiceProvider? _provider;
+
+        public static IServiceProvider Build(IServiceCollection services)
+        {
+            if (services is null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            _provider = services.BuildServiceProvider();
+            return _provider;
+        }
+
+        public static T? GetService<T>()
+        {
+            if (_provider is null)
+            {
+                throw new ArgumentNullException(nameof(_provider), "Ioc provider cannot be null.");
+            }
+
+            var service = _provider.GetService(typeof(T));
+            if (service is null)
+            {
+                throw new ArgumentNullException(nameof(T), "Service was not found.");
+            }
+
+            return (T)service;
+        }
+    }
+}
---
+++ AmethystWindows/DependencyInjection/ViewModelProvider.cs
@@ -0,0 +1,18 @@
+﻿using AmethystWindows.Models;
+
+namespace AmethystWindows.DependencyInjection
+{
+    /// <summary>
+    /// View model locator to pass statically the view model to WPF
+    /// </summary>
+    public class ViewModelProvider
+    {
+        public MainWindowViewModel MainWindowViewModel
+        {
+            get
+            {
+                return IocProvider.GetService<MainWindowViewModel>();
+            }
+        }
+    }
+}
